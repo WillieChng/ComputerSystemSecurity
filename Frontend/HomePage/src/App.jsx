4cb77aa9d1afdf915a4e7123cc55a64a5b8907ff
James <jtee509@gmail.com>
2024-10-13 19:08:30 +0800

    Frontend Reset

--- Frontend/HomePage/src/App.jsx
+++ Frontend/HomePage/src/App.jsx
@@ -1,36 +1,7 @@
-import React, { useState } from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import Login from '../pages/Login.jsx';
-import CreateAccount from '../pages/CreateAccount.jsx';
-import ForgotPassword from '../pages/ForgotPassword.jsx';
-import Booking from '../pages/Booking.jsx';
-import Home from '../pages/Home.jsx';
-import AboutUs from '../pages/AboutUs.jsx';
-import Panel from '../components/Panel.jsx';
 
-export default function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  const handleLogin = () => {
-    setIsLoggedIn(true);
-  };
+function App() {
 
-  return (
-    <BrowserRouter>
-      {isLoggedIn && <Panel />}
-      {/* Define routes for different components */}
-      <Routes>
-        <Route path="/" element={<Home isLoggedIn={isLoggedIn} />} />
-        <Route path="/login" element={<Login onLogin={handleLogin} />} /> 
-        <Route path="/create-account" element={<CreateAccount />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/booking" element={isLoggedIn ? <Booking /> : <Navigate to="/login" />} />
-        <Route path="/aboutus" element={<AboutUs />} />
-      </Routes>
-    </BrowserRouter>
-  );
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+export default App
