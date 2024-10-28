7beaa0e5d4805fe96aa52864ad778938592efe09
James <jtee509@gmail.com>
2024-10-28 19:25:30 +0800

    Frontend Framework Template

---
+++ Frontend/Admin/src/pages/Booking.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Booking = () =>{
+  return (
+    <>
+    <div>
+      <h1>Booking Page</h1>
+      <p>This is the Booking page.</p>
+    </div>
+    </>
+  );
+}
+
+export default Booking;
---
+++ Frontend/HomePage/src/pages/AboutUs.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+const AboutUs = () => {
+  return (
+    <>
+    <div>
+      <h1>About Us</h1>
+      <p>This is the About Us page!</p>
+    </div>
+    </>
+  );
+}
+
+
+export default AboutUs;
