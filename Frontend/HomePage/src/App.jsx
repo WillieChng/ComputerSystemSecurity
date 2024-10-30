8edb39d052716d6ccddd0ad9c78e0d9ae8f49cc4
Jun <junwon822@gmail.com>
2024-10-31 02:33:40 +0800

    can ignore

--- Frontend/HomePage/src/App.jsx
+++ Frontend/HomePage/src/App.jsx
@@ -1,57 +1,22 @@
 import React, { useState } from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
-import './App.css';
-import './Login.css';
-import './CreateAccount.css';
-import './ForgotPassword.css';
-import './Panel.css';
-import './TopBar.css';
 import Login from '../pages/Login';
 import CreateAccount from '../pages/CreateAccount';
 import ForgotPassword from '../pages/ForgotPassword';
 import Panel from '../components/Panel';
-import Panel from '../components/TopBar';
+import TopBar from '../components/TopBar';
 
-function App() {
-  const [menuOpen, setMenuOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
-  };
-
+export default function App() {
   return (
-    <div className="App">
-      <header className="top-nav">
-        <div className="logo">CollabKita</div>
-        <div className="menu-icon" onClick={toggleMenu}>
-          <div className="hamburger"></div>
-        </div>
-      </header>
-
+    <BrowserRouter>
+      <TopBar/>
+      <Panel />
       {/* Define routes for different components */}
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/create-account" element={<CreateAccount />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
       </Routes>
-
-      {/* Sidebar Menu */}
-      {menuOpen && (
-        <nav className="side-menu">
-          <button className="close-btn" onClick={toggleMenu}>X</button>
-          <ul>
-            <li>Workspaces</li>
-            <li>Locations</li>
-            <li>Booking</li>
-            <li>Account</li>
-            <li>
-              <Link to="/" onClick={toggleMenu}>Login</Link>
-            </li>
-          </ul>
-        </nav>
-      )}
-    </div>
+    </BrowserRouter>
   );
 }
-
-export default App;
