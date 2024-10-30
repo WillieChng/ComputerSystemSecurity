32921d3fdc639dd5569f2809ee3fc55dbbfb7264
leonardHD0433 <edwinchuajinrui@gmail.com>
2024-10-30 18:48:27 +0800

    modified frontend to mimic dyno's branch

--- Frontend/HomePage/src/App.jsx
+++ Frontend/HomePage/src/App.jsx
@@ -1,26 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom"; 
-import Panel from "./components/Panel.jsx";
-import Home from "./pages/Home.jsx";
-import Booking from  "./pages/Booking.jsx";
-import AboutUs from "./pages/AboutUs.jsx";
-import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Panel from './components/Panel';
+import Booking from './pages/Booking';
+import AboutUs from './pages/AboutUs';
+import Home from './pages/Home';
 
-export default function App() {
-  return (
-    <BrowserRouter>
-      <Panel />
-      <Routes>
-        {/*Besides the first home page the rest of the pages aren't 
-        so important on the orders or ranking but recommended to be organise*/}
-        <Route path="/" element={<Home />} />      
-        <Route path="booking" element={<Booking />} /> 
-        <Route path="aboutus" element={<AboutUs />} />
-      </Routes>
-    </BrowserRouter>
-  );
+
+function App(){
+    return(
+        <Router>
+            <Panel />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/booking" element={<Booking />} />
+                <Route path="/aboutus" element={<AboutUs />} />
+            </Routes>
+        </Router>
+    )
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+export default App
