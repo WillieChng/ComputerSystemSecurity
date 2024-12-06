efb9000feb54336b103daae8a67d7b7751a12681
leonardHD0433 <edwinchuajinrui@gmail.com>
2024-12-06 14:40:52 +0800

    Added obfuscation for Frontend Admin as well

--- Frontend/Admin/vite.config.js
+++ Frontend/Admin/vite.config.js
@@ -1,9 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import obfuscatorPlugin from 'rollup-plugin-obfuscator';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    process.env.NODE_ENV === 'production' && obfuscatorPlugin({
+      compact: true,
+      controlFlowFlattening: true,
+      deadCodeInjection: true,
+      debugProtection: true,
+      debugProtectionInterval: true,
+      disableConsoleOutput: true,
+    }),
+  ],
   server: {
     port: 5176,
   }
